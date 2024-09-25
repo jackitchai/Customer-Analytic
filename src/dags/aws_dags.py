78c9fdc0d84671611baa3075f07515a7c53c3db2
Kitchai Chaomuangchai <j_ckitchai@outlook.com>
2024-09-25 10:44:40 +0700

    Update code

---
+++ src/dags/aws_dags.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import os
+import requests
+import zipfile
+import logging
+import boto3
+from datetime import datetime,timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.utils import timezone
+# from kaggle.api.kaggle_api_extended import KaggleApi
+
+
+default_args = {
+    'owner' : 'jack',
+    'startdate' : datetime.now()
+}
+
+def postgres_to_s3():
+    #step 1 initiate connection, Define the SQL query to extract data
+    postgres_hook = PostgresHook(
+        postgres_conn_id="postgres_localhost",
+        schema="customer_schema"
+    )
+    conn = postgres_hook.get_conn()
+    cursor = conn.cursor()
+
+    query = """
+        SELECT * 
+        FROM customer_schema.customer_table 
+        WHERE StockCode = '85048'
+        """
+
+    # specify filename
+    csv_file = "/opt/airflow/data/retail_clean.csv"
+
+    # Define the COPY command with the query, CSV format, and headers
+    copy_command = f"COPY ({query}) TO STDOUT WITH CSV HEADER"
+
+    with open(csv_file, "w", encoding='utf-8') as f: # use "w+" to create file if it not exist
+        cursor.copy_expert(copy_command, file=f)
+    # close cursor and connection
+    cursor.close()
+    conn.close()
+
+    # specify desired target file name
+    object_name = "data_postgres_cleaned.csv"
+
+    # retrieve credentials
+    key = pd.read_csv(aws_credentials_path)
+    _aws_access_key_id = key["Access key ID"][0]
+    _aws_secret_access_key = key["Secret access key"][0]
+
+    # authenticate and upload to S3
+    s3 = boto3.client('s3',
+                        aws_access_key_id=_aws_access_key_id,
+                        aws_secret_access_key=_aws_secret_access_key)
+    
+    with open(csv_file, "rb") as f:
+        # s3.upload_fileobj(f, aws_bucket, object_name)
+        s3.put_object(Bucket=aws_bucket, Key=object_name, Body=f)
+    f.close()
+    logging.info(f"Completed extracting data from postgres database loaded to {object_name}")
+
+with DAG(
+    dag_id = "dag_with_postgres_hooks_v01",
+    default_args = default_args
+) as dag:
+    task1 = PythonOperator(
+        task_id = "postgres_to_s3",
+        python_callable=postgres_to_s3
+    )
+    task1
--- src/dags/postgres_dag.py
+++ src/dags/postgres_dag.py
@@ -1,6 +1,43 @@
 from datetime import datetime,timedelta
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+import pandas as pd
+import os 
+
+def load_csv_topostgress():
+    base_path = os.path.abspath(os.path.join(os.getcwd(), 'data'))
+    csv_file_path = os.path.join(base_path, 'retail_clean.csv')
+    df = pd.read_csv(csv_file_path)
+    postgres_hook = PostgresHook(postgres_conn_id='postgres_localhost')
+    conn = postgres_hook.get_conn()
+    cursor = conn.cursor()
+
+    # Insert data from the DataFrame into PostgreSQL
+    for index, row in df.iterrows():
+        insert_query = """
+        INSERT INTO customer_schema.customer_table 
+        (Invoice, StockCode, "Description", Quantity, InvoiceDate, Price, "Customer ID", Country, Revenue) 
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        cursor.execute(insert_query, (
+            row['Invoice'], 
+            row['StockCode'], 
+            row['Description'], 
+            row['Quantity'], 
+            row['InvoiceDate'], 
+            row['Price'], 
+            row['Customer ID'], 
+            row['Country'], 
+            row['Revenue']
+        ))
+
+    
+    conn.commit()
+    cursor.close()
+    conn.close()
+
 
 default_args = {
     'owner' : 'jack',
@@ -23,22 +60,20 @@ with DAG(
             DROP TABLE IF EXISTS customer_schema.customer_table;
 
             CREATE TABLE customer_schema.customer_table (
-                InvoiceNo TEXT,
+                Invoice TEXT,
                 StockCode TEXT,
                 "Description" TEXT,
                 Quantity INT,
                 InvoiceDate TIMESTAMP,
-                UnitPrice FLOAT,
-                CustomerID TEXT,
-                Country TEXT
+                Price FLOAT,
+                "Customer ID" TEXT,
+                Country TEXT,
+                Revenue FLOAT
             );
              """
     )
-    task2 = PostgresOperator(
-        task_id = 'insert_into_table',
-        postgres_conn_id = 'postgres_localhost',
-        sql = """
-            insert into Orders values(1,'pending')
-        """
+    task2 = PythonOperator(
+       task_id = 'load_csv_to_postgres',
+       python_callable= load_csv_topostgress
     )
     task1 >> task2 
